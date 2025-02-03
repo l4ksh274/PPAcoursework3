26c6d8afaa703f1903e54d5a378a1a4e55d03d26
Laksh Patel <laksh270406@gmail.com>
2025-02-03 17:41:23 +0000

    Balancing changes

--- src/Dodo.java
+++ src/Dodo.java
@@ -12,7 +12,7 @@ public class Dodo extends Prey
     // The age to which a ankylosaurus can live
     private static final int MAX_AGE = 15;
     // The likelihood of a dodo breeding
-    private static final double BREEDING_PROBABILITY = 0.45;
+    private static final double BREEDING_PROBABILITY = 0.12;
     // The maximum number of births
     private static final int MAX_LITTER_SIZE = 10;
     
