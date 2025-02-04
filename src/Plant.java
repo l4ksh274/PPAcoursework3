3afb904da708c8b43917772da5ae26066e19a99f
Jiwei-Cao <jiweicao2006@gmail.com>
2025-02-04 11:40:10 +0000

    Miscellaneous

--- src/Plant.java
+++ src/Plant.java
@@ -1,18 +1,10 @@
 public abstract class Plant extends Living {
 
-    protected double height;
-    protected double growthRate;
-
     public Plant(Location location, Field field) {
         super(location, field);
     }
 
     public void act() {
         incrementAge();
-        grow();
-    }
-
-    public void grow() {
-        height *= growthRate;
     }
 }
